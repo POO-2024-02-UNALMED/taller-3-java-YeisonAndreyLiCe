526a13b6d413925a20054498b74bbfb6df5e4b08
Yeison Liscano <yliscanoc@unal.edu.co>
2024-11-17 15:27:33 -0500

    solution

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,110 @@
+package taller3.televisores;
+
+public class TV {
+    
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    private Control control;
+    private static int numTV;
+    
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+        numTV++;
+    }
+    
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    
+    public void setControl(Control control) {
+        this.control = control;
+    }
+    
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+    
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public static void setNumTV(int numTV) {
+        TV.numTV = numTV;
+    }
+    
+    public void turnOn() {
+        estado = true;
+    }
+    
+    public void turnOff() {
+        estado = false;
+    }
+    
+    public void canalUp() {
+        if (estado) {
+            if (canal < 120) {
+                canal++;
+            }
+        }
+    }
+    
+    public void canalDown() {
+        if (estado) {
+            if (canal > 1) {
+                canal--;
+            }
+        }
+    }
+    
+    public void volumenUp() {
+        if (estado) {
+            if (volumen < 7) {
+                volumen++;
+            }
+        }
+    }
+    
+    public void volumenDown() {
+        if (estado) {
+            if (volumen > 0) {
+                volumen--;
+            }
+        }
+    }
+    
+    public Marca getMarca() {
+        return marca;
+    }
+    
+    public int getPrecio() {
+        return precio;
+    }
+    
+    public int getVolumen() {
+        return volumen;
+    }
+    
+    public int getCanal() {
+        return canal;
+    }
+    
+    public Control getControl() {
+        return control;
+    }
+    
+    public static int getNumTV() {
+        return numTV;
+    }
+    
+    public boolean getEstado() {
+        return estado;
+    }
+}
